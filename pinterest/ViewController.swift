c8a3e5b913ccb7e0d6d89c7c5c8fcb402f3434eb
Alumno IDS <alumnoids@static-201-163-84-50.alestra.net.mx>
2019-02-22 13:43:44 -0600

    Initial Commit

---
+++ pinterest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pinterest
+//
+//  Created by Alumno IDS on 22/02/19.
+//  Copyright © 2019 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
